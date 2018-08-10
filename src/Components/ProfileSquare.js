cd3b4451dcdc714f30d07b6fb74c57dea2c465ed
Alberto Carreras <a.carreras.c@gmail.com>
2018-08-10 19:45:27 -0400

    add list of user sqaures to container

--- src/Components/ProfileSquare.js
+++ src/Components/ProfileSquare.js
@@ -1,33 +1,22 @@
 import React from 'react';
 import { withRouter} from 'react-router-dom';
-import { connect } from 'react-redux';
-
-// REDUX PROPS 
-const mapStateToProps = state => {
-    return {
-        username: state.username.charAt(0).toUpperCase() + state.username.slice(1),
-        loggedIn: state.loggedIn,
-        profileImageLink: state.profileImageLink,
-    }
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-    }
-  }
 
 const ProfileSquare = (props) => {
     return (
-        <div>
-            <p className="profile-image-username animated flipInY">{props.username}</p>
+        <div className="profile-image-space">
+            <p className="profile-image-username animated flipInY">{props.username.charAt(0).toUpperCase() + props.username.slice(1)}</p>
             <div className="profile-image-logged animated flipInY"></div>
             <img 
                 className="profile-image-list animated flipInY" 
-                src={`${props.profileImageLink}`}
+                src={
+                    props.profileImageLink !== "undefined"
+                    ? `${props.profileImageLink}` 
+                    : `/assets/avatars/avatar${Math.ceil(Math.random() * Math.floor(4))}.gif`
+                }
                 alt="profile" 
             />
         </div>
     );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(ProfileSquare));
+export default withRouter(ProfileSquare);
--- src/Containers/CentralContainer.js
+++ src/Containers/CentralContainer.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 //COMPONENTS
 import ProfileSquare from "./../Components/ProfileSquare"
 
+// REDUX PROPS 
+const mapStateToProps = state => {
+    return {
+        username: state.username,
+        loggedIn: state.loggedIn,
+        profileImageLink: state.profileImageLink,
+        closestUsers: state.closestUsers,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+    }
+  }
+
 class CentralContainer extends Component {
+
+    generateUserList = (closestUsers) => {
+        return(
+            closestUsers.map(
+                (user) => 
+                <ProfileSquare 
+                    key={user.userId}
+                    username={user.username}
+                    profileImageLink={user.profileImageLink}
+                />
+            )
+        )
+    }
+
     render() {
         return (
             <div className="central-container">
-                <ProfileSquare/>
+                <ProfileSquare 
+                    username={"YOU"}
+                    profileImageLink={this.props.profileImageLink}
+                />
+                {this.generateUserList(this.props.closestUsers)}
             </div>
         );
     }
 }
 
-export default CentralContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(CentralContainer);
