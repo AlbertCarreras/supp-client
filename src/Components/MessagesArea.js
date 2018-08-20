69bd3c67f5240bd42132f5698242a6d3f0fce337
Alberto Carreras <a.carreras.c@gmail.com>
2018-08-19 22:04:06 -0400

    add chat section header and name to message

--- src/Components/MessagesArea.js
+++ src/Components/MessagesArea.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import NewMessageForm from './NewMessageForm';
+import { Icon } from 'semantic-ui-react'
+import { connect } from 'react-redux';
 
-const MessagesArea = ({conversation: { id, title, messages },}) => {
+//ADAPTERS
+import Adapters from './../Adapters/Adapters';
 
-  function orderedMessages (messages) {
-    const sortedMessages = messages.sort(
+// REDUX PROPS 
+const mapStateToProps = state => {
+  return {
+      userId: state.userId,
+  }
+}
+
+const MessagesArea = (props) => {
+
+  function friendUser () {
+    return props.conversation.users.find((u) => u.id !== props.userId).username
+  }
+
+  function orderedMessages () {
+    const sortedMessages = props.conversation.messages.sort(
       (a, b) => new Date(a.created_at) - new Date(b.created_at)
     );
     return sortedMessages.map(message => {
-      return <div key={message.id}>{message.text}</div>;
+      return  <div 
+                key={message.id}
+                className="message-box"
+              >
+                <p>{Adapters.capitalize(message.user.username)}:</p>
+                <p>{}</p>
+                <p>{message.text}</p>
+              </div>;
     });
   };
 
   return (
     <div className="messagesArea">
-      {orderedMessages(messages)}
-      <NewMessageForm conversation_id={id} />
+      <h2 className="heart-message">
+        <Icon 
+        color='teal' 
+        name='chat'
+        /> 
+        with {Adapters.capitalize(friendUser())}
+      </h2>
+      {orderedMessages()}
+      <NewMessageForm conversation_id={props.conversation.id} />
     </div>
   );
 };
 
-export default MessagesArea;
+export default connect(mapStateToProps, null)(MessagesArea);
