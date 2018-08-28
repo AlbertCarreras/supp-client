61b14f84538cb71f1c47437cd9afd9a368176702
Alberto Carreras <a.carreras.c@gmail.com>
2018-08-28 19:12:36 -0400

    fix GITHUB_URL_ROOT

--- src/Adapters/AdapterConstants.js
+++ src/Adapters/AdapterConstants.js
@@ -7,7 +7,7 @@
 export const API_SHORT_ROOT = 'https://supp-server.herokuapp.com';
 export const API_ROOT = 'https://supp-server.herokuapp.com/api/v1';
 export const API_WS_ROOT = 'wss://supp-server.herokuapp.com/api/v1/cable';
-export const GITHUB_URL_ROOT = 'https://github.com/AlbertCarreras/supp-client';
+export const GITHUB_URL_ROOT = 'https://albertcarreras.github.io/supp-client/';
 
 //HEADERS
 export const AUTH_HEADERS_JSON =  {
