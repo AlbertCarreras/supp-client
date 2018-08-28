68c341741ad5fd6ae5fc3612735576035a518187
Alberto Carreras <a.carreras.c@gmail.com>
2018-08-28 13:42:49 -0400

    add heroku links

--- src/Adapters/AdapterConstants.js
+++ src/Adapters/AdapterConstants.js
@@ -1,7 +1,11 @@
 //URLS
-export const API_SHORT_ROOT = 'http://localhost:3000';
-export const API_ROOT = 'http://localhost:3000/api/v1';
-export const API_WS_ROOT = 'ws://localhost:3000/api/v1/cable';
+// export const API_SHORT_ROOT = 'http://localhost:3000';
+// export const API_ROOT = 'http://localhost:3000/api/v1';
+// export const API_WS_ROOT = 'ws://localhost:3000/api/v1/cable';
+
+export const API_SHORT_ROOT = 'https://supp-server.herokuapp.com';
+export const API_ROOT = 'https://supp-server.herokuapp.com/api/v1';
+export const API_WS_ROOT = 'wss://supp-server.herokuapp.com/api/v1/cable';
 
 //HEADERS
 export const AUTH_HEADERS_JSON =  {
