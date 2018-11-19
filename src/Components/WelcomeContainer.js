316e3eeb65078f824f554a09e7941fc623d0ed5f
Alberto Carreras <a.carreras.c@gmail.com>
2018-11-18 19:28:42 -0500

    modified welcome container to reduce amount of photos with minimized window

--- src/Components/WelcomeContainer.js
+++ src/Components/WelcomeContainer.js
@@ -39,28 +39,28 @@ const WelcomeContainer = () => {
             {/* Background images collage */}
             <div className="welcome-container">
                 <div className="row"> 
-                    <div className="column">
+                    <div className="column column-1">
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/helena-lopes-459331-unsplash.jpg"} alt="Photographer: Helena Lopes on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/mike-erskine-144525-unsplash.jpg"} alt="Photographer: Mike Erskine on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/alexis-brown-82988-unsplash.jpg"} alt="Photographer: Alexis Brown on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/ben-duchac-66002-unsplash.jpg"} alt="Photographer: Ben Duchac on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/kelsey-chance-575541-unsplash.jpg"} alt="Photographer: Kelsey Chance on Unsplash"/>
                     </div>
-                    <div className="column">
+                    <div className="column column-2">
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/ben-duchac-66002-unsplash.jpg"} alt="Photographer: Ben Duchac on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/kelsey-chance-575541-unsplash.jpg"} alt="Photographer: Kelsey Chance on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/anthony-intraversato-455600-unsplash.jpg"} alt="Photographer: Anthony Intraversato on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/roman-kraft-669711-unsplash.jpg"} alt="Photographer: Roman Kraft on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/kelsey-chance-575541-unsplash.jpg"} alt="Photographer: Kelsey Chance on Unsplash"/>
                     </div> 
-                    <div className="column">
+                    <div className="column column-3">
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/mike-erskine-144525-unsplash.jpg"} alt="Photographer: Mike Erskine on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/alexis-brown-82988-unsplash.jpg"} alt="Photographer: Alexis Brown on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/roman-kraft-669711-unsplash.jpg"} alt="Photographer: Roman Kraft on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/helena-lopes-459331-unsplash.jpg"} alt="Photographer: Helena Lopes on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/mike-erskine-144525-unsplash.jpg"} alt="Photographer: Mike Erskine on Unsplash"/>
                     </div>
-                    <div className="column">
+                    <div className="column column-4">
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/roman-kraft-669711-unsplash.jpg"} alt="Photographer: Roman Kraft on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/kelsey-chance-575541-unsplash.jpg"} alt="Photographer: Kelsey Chance on Unsplash"/>
                         <img src={GITHUB_URL_ROOT+"/assets/welcome_images/ben-duchac-66002-unsplash.jpg"} alt="Photographer: Ben Duchac on Unsplash"/>  
