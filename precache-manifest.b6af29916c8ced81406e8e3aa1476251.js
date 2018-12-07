df8d800831026b74b3ce9e02f4ce91c9f0a8370e
Alberto Carreras <a.carreras.c@gmail.com>
2018-12-06 19:26:17 -0500

    Updates

---
+++ precache-manifest.b6af29916c8ced81406e8e3aa1476251.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/supp-client/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "9637d04e3d8b4984c31b",
+    "url": "/supp-client/static/css/main.114e6432.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/supp-client/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "8173356101ea4c9fd1d1",
+    "url": "/supp-client/static/js/1.81733561.chunk.js"
+  },
+  {
+    "revision": "95953b55aa136073733e",
+    "url": "/supp-client/static/js/runtime~main.95953b55.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/supp-client/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/supp-client/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/supp-client/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/supp-client/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/supp-client/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/supp-client/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "9637d04e3d8b4984c31b",
+    "url": "/supp-client/static/js/main.9637d04e.chunk.js"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/supp-client/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/supp-client/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/supp-client/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/supp-client/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/supp-client/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/supp-client/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/supp-client/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/supp-client/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8173356101ea4c9fd1d1",
+    "url": "/supp-client/static/css/1.27418edd.chunk.css"
+  },
+  {
+    "revision": "21f82d7a6b1e59ce839014172ac2b795",
+    "url": "/supp-client/index.html"
+  }
+];
