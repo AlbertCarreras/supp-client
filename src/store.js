08ee8e77c2fb27b18b052a1cac480fdf9aa75f77
Alberto Carreras <a.carreras.c@gmail.com>
2019-03-06 00:36:20 -0500

    refactored store.js for cleaner code

--- src/store.js
+++ src/store.js
@@ -3,16 +3,14 @@ import {applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
-const store =  process.env.NODE_ENV === 'development' 
-? createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ 
-    && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk, logger), 
-  ) 
-: createStore(
-    reducer,
-    applyMiddleware(thunk), 
-  );
+let middleWare = [ thunk ]
+let storeArgs = [ reducer, applyMiddleware(...middleWare) ]
+
+if (process.env.NODE_ENV === 'development') {
+  middleWare.push(logger)
+  storeArgs.push(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+}
   
-  export default store;
+const store = createStore(...storeArgs)
+
+export default store;
